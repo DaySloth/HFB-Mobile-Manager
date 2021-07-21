d6ee6d52639836d17201305398cb11d3b7558d02
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-07-21 18:21:40 -0500

    Update index.js

--- pages/products/index.js
+++ pages/products/index.js
@@ -52,7 +52,7 @@ export default function Products({ products }) {
           </div>
 
           <hr />
-          {products ? (
+          {products[0] ? (
             <>
               {/* <div className={styles.container}>
                 <Input
