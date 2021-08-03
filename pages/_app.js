12049f851d72820a5ed6876591889011cae933c6
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-08-03 14:42:02 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
 import { Provider } from "next-auth/client";
+import ThemeContext from "../util/context/darkTheme";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
-    return (
-        <Provider session={pageProps.session}>
-            <Component {...pageProps} />
-        </Provider>
-    );
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <Provider session={pageProps.session}>
+      <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+        <Component {...pageProps} />
+      </ThemeContext.Provider>
+    </Provider>
+  );
 }
